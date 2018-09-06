a8e23e665c3dcfe4c2990fa02dff07be6bd51ccb
Dennis Vinther Jensen <dennis.jensen@idnet.dk>
2018-09-07 00:30:08 +0200

    Solved maxchar and fizzbuzz

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,29 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let max = 0;
+  let maxChar = '';
+  let charMap = {};
+
+  // for loop below seems more readable and less code than reduce
+  // const charMap = str.split('').reduce((obj, char) => {
+  //   obj[char] = obj[char]++ || 1;
+  //   return obj;
+  // }, {});
+
+  for (let char of str) {
+    charMap[char] = charMap[char]++ || 1;
+  }
+
+  for (let char in charMap) {
+    if (charMap[char] > max) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+
+  return maxChar;
+}
 
 module.exports = maxChar;
