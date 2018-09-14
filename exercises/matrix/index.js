04bee61106231e286fedf73c45f34c69be346d41
Dennis Vinther Jensen <dennis.jensen@idnet.dk>
2018-09-14 10:21:34 +0200

    Added solutions

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,108 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let startRow = 0;
+  let endRow = n - 1;
+  let startCol = 0;
+  let endCol = n - 1;
+  let counter = 1;
+  const grid = Array.apply(null, Array(n)).map(_ => []);
+  while (startRow <= endRow && startCol <= endCol) {
+
+    // Top row
+    for (let i = startCol; i <= endCol; i++) {
+      grid[startRow][i] = counter;
+      counter++;
+    }
+    startRow++;
+
+    // Right column
+    for (let i = startRow; i <= endRow; i++) {
+      grid[i][endCol] = counter;
+      counter++;
+    }
+    endCol--;
+
+    // Bottom row
+    for (let i = endCol; i >= startCol; i--) {
+      grid[endRow][i] = counter;
+      counter++;
+    }
+    endRow--;
+
+    // Left column
+    for (let i = endRow; i >= startRow; i--) {
+      grid[i][startCol] = counter;
+      counter++;
+    }
+    startCol++;
+
+  }
+
+  return grid;
+
+}
+
 
 module.exports = matrix;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let startRow = 0;
+//   let endRow = n - 1;
+//   let startCol = 0;
+//   let endCol = n - 1;
+//   let counter = 1;
+//   let results = Array.apply(null, Array(n)).map(elm => []);
+//   //     1           1         2           1
+//   while (startCol <= endCol && startRow <= endRow) {
+//     // Top row
+//     for (let col = startCol; col <= endCol; col++) {
+//       results[startRow][col] = counter;
+//       counter++;
+//     }
+//     startRow++;
+//     // Right col
+//     for (let row = startRow; row <= endRow; row++) {
+//       results[row][endCol] = counter;
+//       counter++;
+//     }
+//     endCol--;
+//     // Bottom row
+//     for (let col = endCol; col >= startCol; col--) {
+//       results[endRow][col] = counter;
+//       counter++;
+//     }
+//     endRow--;
+//     // Left col
+//     for (let row = endRow; row >= startRow; row--) {
+//       results[row][startCol] = counter;
+//       counter++;
+//     }
+//     startCol++
+//   }
+//   return results;
--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,22 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+
+  // 1 - Best readability and shortest
+  const matches = str.match(/[aeiou]/gi);
+  return matches ? matches.length : 0;
+
+  // 2
+  // let vowelsCount = 0;
+  // const vowelChars = ['a', 'e', 'i', 'o', 'u'];
+  // for (let char of str.toLowerCase()) {
+  //   if (vowelChars.includes(char)) {
+  //     vowelsCount++
+  //   }
+  // }
+  // return vowelsCount;
+
+}
 
 module.exports = vowels;
